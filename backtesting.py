83350f2c95a7b3fd319ee685b40c405e88b96664
Dmitry Mikhaylov <dmik@vt.edu>
2020-07-17 11:14:59 -0400

    finish strategy class

--- backtesting.py
+++ backtesting.py
@@ -1,5 +1,5 @@
 """
-Part of my robo-advisor library for portfolio management of market tradable securities,
+Part of robo-advisor library for portfolio management of market tradable securities,
 imports to help with portfolio construction, optimization, allocation,and rebalancing,
 taking into account individual risk tolerance and preferred choices of investment vehicles.
 """

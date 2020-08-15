77e25598c8a983f9dbe20e6c56e0d0b2cc1b335a
Dmitry Mikhaylov <dmik@vt.edu>
2020-08-15 09:01:51 -0400

    searching better place 3

---
+++ helpers.py
@@ -0,0 +1,15 @@
+
+def extract_coupon(description):
+    coupon = ''
+    for s in range(len(description)):
+        if description[s].isnumeric() or description[s] == '.':
+            coupon =  coupon + description[s]
+            #print(c)
+        if description[s] == '%':
+            break 
+    try:
+        float(coupon)
+    except:
+        coupon = coupon[1:]
+        float(coupon)
+    return coupon
